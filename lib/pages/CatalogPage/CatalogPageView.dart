5c9571c04a6c8c2684eae4275971ea5e0676b4b7
“altharaa” <aletheamaureen01@gmail.com>
2023-11-20 10:27:10 +0700

    style : [AM] stylig catalog app bar

--- lib/pages/CatalogPage/CatalogPageView.dart
+++ lib/pages/CatalogPage/CatalogPageView.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:metal_marketplace/pages/CatalogPage/Components/catalog_component_one.dart';
 
 class CatalogPage extends StatelessWidget {
   const CatalogPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        CatalogComponentOne()
+      ],
+    );
   }
 }
--- lib/pages/HomePage/components/home_component_two.dart
+++ lib/pages/HomePage/components/home_component_two.dart
@@ -14,51 +14,29 @@ class HomeComponentTwo extends StatelessWidget {
     final double width = mediaQuery.width;
     final double height = mediaQuery.height;
 
-    return Container(
-        padding: EdgeInsets.only(left: 25, right: 25),
-        child: SingleChildScrollView(
-            child: Column(children: [
-          Container(
-            width: 340,
-            margin: EdgeInsets.only(top: 30),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(25),
+    return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: CarouselSlider.builder(
+            itemCount: 3,
+            options: CarouselOptions(
+              autoPlay: true,
+              viewportFraction: 0.7,
+              enlargeCenterPage: true,
+              enlargeFactor: 0.13,
+              pageSnapping: true,
+              autoPlayCurve: Curves.linearToEaseOut,
+              autoPlayAnimationDuration: Duration(seconds: 3),
             ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(25),
-              child: CarouselSlider(
-                items: [promote1, promote2, promote3, promote4].map((i) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Container(
-                        width: 340,
-                        height: 300,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(25),
-                          image: DecorationImage(
-                            image: AssetImage(i),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                }).toList(),
-                options: CarouselOptions(
-                  height: 193,
-                  initialPage: 0,
-                  enableInfiniteScroll: true,
-                  autoPlay: true,
-                  autoPlayInterval: Duration(seconds: 3),
-                  autoPlayAnimationDuration: Duration(milliseconds: 800),
-                  autoPlayCurve: Curves.fastOutSlowIn,
-                  enlargeFactor: 0.3,
-                  enlargeCenterPage: true,
-                  scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index, realIndex) => ClipRRect(
+              borderRadius: BorderRadius.circular(40),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 3),
+                child: Image.asset(
+                    caribea
                 ),
               ),
-            ),
-          )
-        ])));
+            )
+        )
+    );
   }
 }
